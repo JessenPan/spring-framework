d93d7d3de53c3ee671d7c008379dc105e01aa7dd
jessenpan <cdpanxinyu@jd.com>
2019-09-18 11:17:29 +0800

    init

--- code-comment/comment-annotation/src/main/java/org/jessenpan/spring/comment/annotation/DesignPrincipleEnum.java
+++ code-comment/comment-annotation/src/main/java/org/jessenpan/spring/comment/annotation/DesignPrincipleEnum.java
@@ -11,7 +11,13 @@ import lombok.ToString;
 @ToString
 public enum DesignPrincipleEnum {
 
-    SRP(1, "职责单一原则");
+    SRP(1, "职责单一原则"),
+    OCP(2, "开闭原则"),
+    LSP(3, "里氏替换原则"),
+    ISP(4, "接口隔离原则"),
+    DIP(5, "依赖注入原则"),
+
+    LKP(6, "最少知道原则");
 
     private int code;
     private String desc;
